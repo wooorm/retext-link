22067685d15edfe267e552702dd4f4462509ab0b
wooorm <tituswormer@gmail.com>
2014-09-26 11:11:06 +0200

    Refactor API

--- index.js
+++ index.js
@@ -1,40 +1,88 @@
 'use strict';
 
-function astToString(node) {
-    if ('value' in node) {
-        return node.value;
+/**
+ * Define `link`.
+ */
+
+function link() {}
+
+/**
+ * Constants.
+ */
+
+var EXPRESSION_SLASH;
+
+EXPRESSION_SLASH = /^\/{1,3}$/;
+
+/**
+ * Stringify an NLCST node.
+ *
+ * @param {CSTNode}
+ * @return {string}
+ */
+
+function cstToString(token) {
+    var value,
+        index,
+        children;
+
+    /* istanbul ignore if */
+    if (token.value) {
+        return token.value;
     }
 
-    if (node.children.length === 1) {
-        return astToString(node.children[0]);
+    children = token.children;
+
+    /* Shortcut: This is pretty common, and a small performance win. */
+    if (children.length === 1 && children[0].type === 'TextNode') {
+        return children[0].value;
     }
 
-    var values = [],
-        children = node.children,
-        iterator = -1;
+    value = [];
+    index = -1;
 
-    while (children[++iterator]) {
-        values.push(astToString(children[iterator]));
+    while (children[++index]) {
+        value[index] = cstToString(children[index]);
     }
 
-    return values.join('');
+    return value.join('');
 }
 
-var EXPRESSION_SLASH = /^\/{1,3}$/;
+/**
+ * Merge punctuation marks and words into a source node.
+ *
+ * @param {CSTNode} child
+ * @param {number} index
+ * @param {CSTNode} parent
+ * @return {undefined|number} - Either void, or the next index to iterate
+ *   over.
+ */
 
 function mergeLinkExceptions(child, index, parent) {
-    var siblings = parent.children,
-        nodes = [child],
-        start, end, currentIndex, value, type;
+    var siblings,
+        nodes,
+        start,
+        end,
+        currentIndex,
+        value,
+        type;
+
+    siblings = parent.children;
+    nodes = [child];
 
     if (
         child.type !== 'PunctuationNode' ||
-        astToString(child) !== '.'
+        cstToString(child) !== '.'
     ) {
         return;
     }
 
     currentIndex = end = start = index;
+
+    /**
+     * Find preceding word/punctuation.
+     * Stop before slashes, break after `www`.
+     */
 
     while (siblings[start - 1]) {
         type = siblings[start - 1].type;
@@ -45,20 +93,26 @@ function mergeLinkExceptions(child, index, parent) {
 
         if (
             type === 'PunctuationNode' &&
-            EXPRESSION_SLASH.test(astToString(siblings[start - 1]))
+            EXPRESSION_SLASH.test(cstToString(siblings[start - 1]))
         ) {
             break;
         }
 
-        nodes.unshift(siblings[--start]);
+        start--;
+
+        nodes.unshift(siblings[start]);
 
         if (
             type === 'WordNode' &&
-            astToString(siblings[start]) === 'www'
+            cstToString(siblings[start]) === 'www'
         ) {
             break;
         }
     }
+
+    /**
+     * Find following word/punctuation.
+     */
 
     while (siblings[end + 1]) {
         type = siblings[end + 1].type;
@@ -67,72 +121,111 @@ function mergeLinkExceptions(child, index, parent) {
             break;
         }
 
-        nodes.push(siblings[++end]);
+        end++;
+
+        nodes.push(siblings[end]);
     }
+
+    /**
+     * This full stop doesnt look like a link:
+     * it's either not followed, or not preceded,
+     * by words or punctuation.
+     */
 
     if (currentIndex === start || currentIndex === end) {
         return;
     }
 
-    /* 1-3 slashes */
+    /**
+     * 1-3 slashes.
+     */
+
     if (
         start > 0 &&
         siblings[start - 1].type === 'PunctuationNode' &&
-        EXPRESSION_SLASH.test(astToString(siblings[start - 1]))
+        EXPRESSION_SLASH.test(cstToString(siblings[start - 1]))
     ) {
-        nodes.unshift(siblings[--start]);
+        start--;
+
+        nodes.unshift(siblings[start]);
     }
 
-    /* URL protocol and colon. */
+    /**
+     * URL protocol and colon.
+     */
+
     if (
         start > 2 &&
         siblings[start - 1].type === 'PunctuationNode' &&
-        astToString(siblings[start - 1]) === ':' &&
+        cstToString(siblings[start - 1]) === ':' &&
         siblings[start - 2].type === 'WordNode'
     ) {
         nodes.unshift(siblings[start - 2], siblings[start - 1]);
         start -= 2;
     }
 
-    value = astToString(siblings[end]);
+    /**
+     * Remove the last node if it's punctuation,
+     * unless it's `/` or `)`.
+     */
+
+    value = cstToString(siblings[end]);
 
     if (
         siblings[end].type === 'PunctuationNode' &&
         value !== '/' && value !== ')'
     ) {
         end--;
+
         nodes.pop();
     }
+
+    /**
+     * Remove the nodes and insert a SourceNode.
+     */
 
     siblings.splice(start, end - start + 1, {
         'type' : 'SourceNode',
         'data' : {
             'dataType' : 'link'
         },
-        'value' : astToString({
+        'value' : cstToString({
             'children' : nodes
         })
     });
 
     index++;
 
+    /**
+     * Ignore the following full-stop: it's not part
+     * of a link.
+     */
+
     if (value === '.') {
-        return index++;
+        index++;
     }
 
     return index;
 }
 
-function attach(retext) {
-    var parser = retext.parser;
+/**
+ * Define `attach`.
+ *
+ * @param {Retext} retext
+ */
 
-    parser.tokenizeSentenceModifiers = [
-            mergeLinkExceptions
-        ].concat(parser.tokenizeSentenceModifiers);
+function attach(retext) {
+    retext.parser.tokenizeSentenceModifiers.unshift(mergeLinkExceptions);
 }
 
-function link() {}
-
-exports = module.exports = link;
+/**
+ * Expose `attach`.
+ */
 
 link.attach = attach;
+
+/**
+ * Expose `link`.
+ */
+
+module.exports = link;
